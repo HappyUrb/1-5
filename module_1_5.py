b3bf31d7895855adbeb907cd89055a390cd0e4f6
HappyUrb <nik.skutin205@gmail.com>
2024-11-09 19:28:31 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = 1, 2, 'Практика'
+print (immutable_var)
+print ('Кортеж не поддерживает обращение по элементам, поэтому нельзя изменить его значение')
+mutable_list = ([1, 2, 'Практика'])
+mutable_list[2] = 'Замена'
+print (mutable_list)
